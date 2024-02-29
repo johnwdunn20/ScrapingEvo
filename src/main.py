681af63fd4a607bed16236562cf1700921b602b1
John Dunn <johnwdunn20@gmail.com>
2024-02-28 19:59:32 -0700

    updated class

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import pandas as pd
 from random import randint
 from time import sleep
 import math
+from datetime import datetime
+
 
 class ScrapeEvoSkis():
     def __init__(self, csv_location=None):
@@ -13,10 +15,15 @@ class ScrapeEvoSkis():
             self.csv_location = os.getcwd() + '/raw_data.csv'
         else:
             self.csv_location = csv_location
+        # Store last scraped date
+        self.last_scraped = None
 
     def scrape(self, url, count_total_skis, printURls=False):
         print("Scraping Evo Skis")
         # add logic from ScrapeEvoSkis.py
+        
+        # update last scraped date if successful
+        self.last_scraped = datetime.now()
         
     def scrape_threads(self, url, count_total_skis, printURls=False):
         pass
