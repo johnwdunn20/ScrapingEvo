bf98491d52a9c064d35b0b2151a825737be58e7f
John Dunn <johnwdunn20@gmail.com>
2024-03-08 14:49:25 -0700

    updated example

--- src/example.py
+++ src/example.py
@@ -6,5 +6,5 @@ twin_tip = ScrapeEvoSkis()
 # setting count_total_skis to 40 for testing purposes - will only get the first page
 # twin_tip.scrape(url='https://www.evo.com/shop/ski/skis/twin-tip', count_total_skis=40, printURls=True)
 
-# search csv for skis
+# search csv for skis (returns the ski name and url)
 twin_tip.search(keywords=['RMU'])
